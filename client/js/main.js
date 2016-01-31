c6dc6b8533d2ba1cd22ade6d9bd874fc1d15e371
DanLombardy <lombardy.dan@gmail.com>
2016-01-31 00:56:34 -0800

    Modify deal seed to fully work

--- client/js/main.js
+++ client/js/main.js
@@ -8,21 +8,22 @@ var airportData = "";
 
   socket.on('potentialAdventures', function(data){
 		var potentialAdventures = data;
-		  
+
 		  if(!data){
 			  //handle a failure
 		  }
-		  
+
 		  //remove this page and load next stuff
 		  console.log("change to next page");
+      console.log(data);
 		  var t='<div id="selectionMenu"><div id="leftPanel"></div></div><div id="holder"><h2>Below are a list of locations that match your budget.<br/>Click to customize the activities you can do!</h2></div>';
 		  $('#container').html(t);
-		  
-		  //load the page elements 
-				
+
+		  //load the page elements
+
 		  entryform(document.getElementById('leftPanel'), "sidePanel");
 		  var cities = ["NewYork","Seattle","Chicago","LosAngeles","Atlanta","Orlando","DallasFortWorth","Denver","Miami","Newark","SanFrancisco","Houston","Beijing","Dubai","Tokyo","London","HongKong","Bangkok","Shanghai","Paris","Guangzhou","Singapore","Istanbul","Seoul","Frankfurt","Amsterdam","Delhi","SaoPaulo","Johannesburg","Cairo","Moscow","Sydney","Prague","Rome"];
-						
+
 			for(var i=0; i<cities.length;i++){
 				var str = '<div id="'+cities[i]+'Viewer" class="cityViewer">';
 				str+='<div class="watermark"><img src="imgs/adventureMe_white.png"/></div>';
@@ -34,25 +35,25 @@ var airportData = "";
 			}
 		$(function() {
 		   	$('.cityViewer').mouseover(function() {
-			
+
 			  var elem = document.getElementById(this.id).getElementsByClassName("topImage");
-			 
+
 			  $(elem[0]).css("visibility","hidden");
 			});
-			
+
 			$('.cityViewer').mouseout(function() {
-			
+
 			  var elem = document.getElementById(this.id).getElementsByClassName("topImage");
 					  $(elem[0]).css("visibility","visible");
 			});
 			setDates();
 		});
-		  
+
  });//on potential adventures
 
 $(function() {
-	
-	
+
+
 	//load airport data
 	$.getScript( "airportdata.json", function( data, textStatus, jqxhr ) {
 	  airportData = JSON.parse(data);
@@ -61,21 +62,21 @@ $(function() {
 	var t= '<select id="departureCity">';
 	  for(port in airportData){
 		  var loc = airportData[port].City+", "+airportData[port].Country;
-		 
+
 		  t+='<option value="'+airportData[port].IATA+'">'+loc+'</option>';
-						
+
 	  }
-	  
+
 	  t+= '</select>';
-	  
+
 	   airportData= t;
-	 
+
 	  //then after loading add the damn form
-	  
+
 	  entryform(document.getElementById('entryForm'), "frontpage");
 	  setDates();
 	});
-     
+
 });
 
 function setDates(){
@@ -83,24 +84,23 @@ function setDates(){
 	var dd = today.getDate();
 	var mm = today.getMonth()+1; //January is 0!
 	var yyyy = today.getFullYear();
-	
+
 	if(dd<10) {
 	    dd='0'+dd
-	} 
-	
+	}
+
 	if(mm<10) {
 	    mm='0'+mm
-	} 
-	
+	}
+
 	today = mm+'/'+dd+'/'+yyyy;
 	if(leavet=="")
 		leavet = today;
 	if(returnt=="")
 		returnt=today;
-		
+
     $( "#leaving" ).datepicker();
     $( "#leaving" ).datepicker("setDate", leavet);
     $( "#returning" ).datepicker();
     $( "#returning" ).datepicker("setDate", returnt);
 }
-
