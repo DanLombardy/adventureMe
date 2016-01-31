053da4b3699cf37024eeab940c5ac4c8ea4a13d1
Amanda Keogh <amanda.keogh@gmail.com>
2016-01-31 09:26:22 -0800

    add airport hash, fix sorter

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ var getAdventureDeals = function(budget, startDate, endDate, lengthOfStay, origi
   console.log(budget, startDate, endDate, lengthOfStay, originTLA, personCount);
   seedDeals(budget, startDate, endDate, lengthOfStay, originTLA, function(cities) {
     cities.sort(function(deals1, deals2) {
-      var deals = airportRelationships[deals2[0].destinationTLA] - airportRelationships[deals1[0].destinationTLA];
+      return airportRelationships[deals2[0].destinationTLA] - airportRelationships[deals1[0].destinationTLA];
     });
     var deals = {deals: cities.slice(0,9)};
     console.log(deals);
